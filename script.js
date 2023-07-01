e6c149a110b8ae110f11ca89b92917b4ced6edd3
Aydin Delong <138320647+A-Person-IRL@users.noreply.github.com>
2023-07-01 16:20:23 -0500

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('scroll', function() {
-  var parallax = document.querySelector('#hero');
-  var scrolled = window.pageYOffset;
-  parallax.style.transform = 'translateY(' + (scrolled * 0.5) + 'px)';
-});
