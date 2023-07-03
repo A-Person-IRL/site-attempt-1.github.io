ee03e8e29896a0fcb9c08c127133627cdb5f18ce
Aydin Delong <138320647+A-Person-IRL@users.noreply.github.com>
2023-07-03 15:46:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
   });
 });
+
+// Change navigation background on scroll
+window.addEventListener('scroll', function() {
+  const nav = document.querySelector('nav');
+  if (window.scrollY > 0) {
+    nav.style.background = 'rgba(0, 0, 0, 0.8)';
+  } else {
+    nav.style.background = 'transparent';
+  }
+});
