9db35e8fa0464ed4da5efc573406806890a58b77
Aydin Delong <138320647+A-Person-IRL@users.noreply.github.com>
2023-07-03 16:18:31 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,71 +1,32 @@
-// Smooth scrolling with faster background fade
+// Smooth scrolling
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
 
-    const target = document.querySelector(this.getAttribute('href'));
-    const offset = target.offsetTop;
-
-    window.scrollTo({
-      top: offset,
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
       behavior: 'smooth'
-    });
-
-    // Update background color during scroll
-    const sections = document.querySelectorAll('section');
-    const homeSection = document.getElementById('home');
-    const nav = document.querySelector('nav');
-
-    let lastKnownScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-    let ticking = false;
-
-    function changeBackground() {
-      const windowHeight = window.innerHeight;
-      const navHeight = nav.offsetHeight;
-      const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-      const scrollBottom = scrollPosition + windowHeight - navHeight;
-      const scrollThreshold = 0.5; // Adjust the threshold as desired
-
-      sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.offsetHeight;
-
-        if (scrollBottom >= sectionTop && scrollBottom < sectionTop + sectionHeight) {
-          const progress = (scrollBottom - sectionTop) / sectionHeight;
-          const inverseProgress = 1 - progress;
-
-          // Adjust the opacity and background color based on the inverse progress
-          const opacity = inverseProgress * 0.8;
-          const backgroundColor = `rgba(0, 0, 0, ${opacity})`;
-
-          homeSection.style.backgroundColor = backgroundColor;
-          nav.style.backgroundColor = backgroundColor;
-        }
-      });
-    }
-
-    window.addEventListener('scroll', function () {
-      lastKnownScrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-
-      if (!ticking) {
-        window.requestAnimationFrame(function () {
-          changeBackground();
-          ticking = false;
-        });
-
-        ticking = true;
-      }
     });
   });
 });
 
 // Change navigation background on scroll
-window.addEventListener('scroll', function () {
+window.addEventListener('scroll', function() {
   const nav = document.querySelector('nav');
-  if (window.scrollY > 0) {
-    nav.style.background = 'rgba(0, 0, 0, 0.8)';
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
+  if (scrollPosition > 0) {
+    nav.style.background = `rgba(0, 0, 0, ${scrollPosition / 500})`;
   } else {
-    nav.style.background = 'transparent';
+    nav.style.background = 'rgba(0, 0, 0, 0)';
   }
 });
+
+// Change background color on home section
+const homeSection = document.getElementById('home');
+let colorIndex = 0;
+
+setInterval(() => {
+  const colors = ['#000', '#141414', '#222', '#333']; // Add more colors here
+  colorIndex = (colorIndex + 1) % colors.length;
+  homeSection.style.backgroundColor = colors[colorIndex];
+}, 5000);
