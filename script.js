8bf859b41f48403bc287c643c3b68a4d6e65d67d
Aydin Delong <138320647+A-Person-IRL@users.noreply.github.com>
2023-07-04 17:48:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-// Smooth scrolling with background color change
-const sections = document.querySelectorAll('section');
-let currentSection = sections[0];
-
-function changeBackgroundColor() {
+// Change navigation background on scroll
+window.addEventListener('scroll', function () {
+  const nav = document.querySelector('nav');
+  const bodyBackground = document.querySelector('body::before');
+  const sections = document.querySelectorAll('section');
+  const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
   const windowHeight = window.innerHeight;
-  const scrollPosition = window.scrollY || window.pageYOffset;
 
-  for (let i = 0; i < sections.length; i++) {
-    const section = sections[i];
-    const sectionOffset = section.offsetTop;
-    const sectionHeight = section.offsetHeight;
+  const currentSectionIndex = Math.floor(scrollPosition / windowHeight);
+  const currentSection = sections[currentSectionIndex];
+  const nextSection = sections[currentSectionIndex + 1];
 
-    if (
-      scrollPosition >= sectionOffset - windowHeight / 4 &&
-      scrollPosition < sectionOffset + sectionHeight - windowHeight / 4
-    ) {
-      const bgColor = section.getAttribute('data-bg-color');
-      document.body.style.backgroundColor = bgColor;
-      currentSection = section;
-      break;
-    }
-  }
+  const currentSectionMiddle = currentSection.offsetTop + currentSection.offsetHeight / 2;
+  const nextSectionMiddle = nextSection.offsetTop + nextSection.offsetHeight / 2;
+
+  const fadeStartPosition = currentSection.offsetTop - windowHeight / 4;
+  const fadeEndPosition = nextSection.offsetTop - windowHeight / 4;
+
+  const scrollPercentage = (scrollPosition - fadeStartPosition) / (fadeEndPosition - fadeStartPosition);
+  const fadeOpacity = Math.max(0, Math.min(1, scrollPercentage));
+
+  const currentColor = currentSection.style.backgroundColor || window.getComputedStyle(currentSection).backgroundColor;
+  const nextColor = nextSection.style.backgroundColor || window.getComputedStyle(nextSection).backgroundColor;
+
+  const targetColor = blendColors(currentColor, nextColor, fadeOpacity);
+
+  nav.style.backgroundColor = targetColor;
+  bodyBackground.style.backgroundColor = targetColor;
+});
+
+// Blend two colors based on opacity
+function blendColors(color1, color2, opacity) {
+  const parseColor = (color) => color.match(/\d+/g).map(Number);
+  const formatColor = (color) => `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+
+  const rgb1 = parseColor(color1);
+  const rgb2 = parseColor(color2);
+
+  const blendedColor = rgb1.map((value, index) => Math.round(value + (rgb2[index] - value) * opacity));
+  return formatColor(blendedColor);
 }
-
-window.addEventListener('scroll', changeBackgroundColor);
-
