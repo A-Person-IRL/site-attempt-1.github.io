fbb1d3c2d676ef7e25976c3403538e95c3695190
Aydin Delong <138320647+A-Person-IRL@users.noreply.github.com>
2023-07-04 18:01:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,56 @@
-// Change navigation background on scroll
-window.addEventListener('scroll', function () {
-  const nav = document.querySelector('nav');
-  const bodyBackground = document.querySelector('body::before');
+// Calculate fade positions based on scroll position
+window.addEventListener('scroll', function() {
   const sections = document.querySelectorAll('section');
   const scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
-  const windowHeight = window.innerHeight;
+  const viewportHeight = window.innerHeight;
+  const fadeStart = viewportHeight * 0.25;
+  const fadeEnd = viewportHeight * 0.75;
+  let currentSection, nextSection, fade;
 
-  const currentSectionIndex = Math.floor(scrollPosition / windowHeight);
-  const currentSection = sections[currentSectionIndex];
-  const nextSection = sections[currentSectionIndex + 1];
+  // Find current and next sections
+  for (let i = 0; i < sections.length; i++) {
+    const sectionOffset = sections[i].offsetTop;
+    const sectionHeight = sections[i].offsetHeight;
 
-  const currentSectionMiddle = currentSection.offsetTop + currentSection.offsetHeight / 2;
-  const nextSectionMiddle = nextSection.offsetTop + nextSection.offsetHeight / 2;
+    if (scrollPosition >= sectionOffset - fadeStart && scrollPosition < sectionOffset + sectionHeight - fadeEnd) {
+      currentSection = sections[i];
+      nextSection = sections[i + 1];
+      break;
+    }
+  }
 
-  const fadeStartPosition = currentSection.offsetTop - windowHeight / 4;
-  const fadeEndPosition = nextSection.offsetTop - windowHeight / 4;
+  // Calculate fade opacity
+  if (currentSection && nextSection) {
+    const currentSectionMiddle = currentSection.offsetTop + currentSection.offsetHeight / 2;
+    const nextSectionMiddle = nextSection.offsetTop + nextSection.offsetHeight / 2;
 
-  const scrollPercentage = (scrollPosition - fadeStartPosition) / (fadeEndPosition - fadeStartPosition);
-  const fadeOpacity = Math.max(0, Math.min(1, scrollPercentage));
+    if (scrollPosition < currentSectionMiddle) {
+      fade = 1 - (currentSectionMiddle - scrollPosition) / fadeStart;
+    } else {
+      fade = 1 - (scrollPosition - currentSectionMiddle) / fadeStart;
+    }
 
-  const currentColor = currentSection.style.backgroundColor || window.getComputedStyle(currentSection).backgroundColor;
-  const nextColor = nextSection.style.backgroundColor || window.getComputedStyle(nextSection).backgroundColor;
+    fade = Math.max(0, Math.min(1, fade)); // Clamp fade value between 0 and 1
+  }
 
-  const targetColor = blendColors(currentColor, nextColor, fadeOpacity);
-
-  nav.style.backgroundColor = targetColor;
-  bodyBackground.style.backgroundColor = targetColor;
+  // Update background color based on fade
+  blendColors('#000', '#fff', fade); // Change the colors as needed
 });
 
-// Blend two colors based on opacity
-function blendColors(color1, color2, opacity) {
-  const parseColor = (color) => color.match(/\d+/g).map(Number);
-  const formatColor = (color) => `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
+// Blend background colors
+function blendColors(color1, color2, ratio) {
+  const r1 = parseInt(color1.substring(1, 3), 16);
+  const g1 = parseInt(color1.substring(3, 5), 16);
+  const b1 = parseInt(color1.substring(5, 7), 16);
 
-  const rgb1 = parseColor(color1);
-  const rgb2 = parseColor(color2);
+  const r2 = parseInt(color2.substring(1, 3), 16);
+  const g2 = parseInt(color2.substring(3, 5), 16);
+  const b2 = parseInt(color2.substring(5, 7), 16);
 
-  const blendedColor = rgb1.map((value, index) => Math.round(value + (rgb2[index] - value) * opacity));
-  return formatColor(blendedColor);
+  const r = Math.round(r1 + (r2 - r1) * ratio);
+  const g = Math.round(g1 + (g2 - g1) * ratio);
+  const b = Math.round(b1 + (b2 - b1) * ratio);
+
+  const blendedColor = `rgb(${r}, ${g}, ${b})`;
+  document.body.style.backgroundColor = blendedColor;
 }
