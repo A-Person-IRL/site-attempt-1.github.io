90a590f1ea30658855139585ed3338d808efec7b
Aydin Delong <138320647+A-Person-IRL@users.noreply.github.com>
2023-07-04 15:43:12 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
 
-    const targetSection = document.querySelector(this.getAttribute('href'));
-    const targetColor = window.getComputedStyle(targetSection).backgroundColor;
-
-    document.querySelector('html, body').scrollTo({
-      top: targetSection.offsetTop,
-      behavior: 'smooth'
-    });
-
-    animateBackground(targetColor);
-  });
-});
